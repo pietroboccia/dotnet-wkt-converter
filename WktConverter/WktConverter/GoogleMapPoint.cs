0f7c7ce5c34bbd9612b273f09e4c089929cc2260
Pietro Boccia <pietro.boccia@avanade.com>
2025-06-13 15:19:31 +0200

    feat(wkt): convert a wkt string into a List of List of points lat and long

---
+++ WktConverter/WktConverter/GoogleMapPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WktConverter;
+
+public class GoogleMapPoint
+{
+    [JsonPropertyName("lat")]
+    public double Latitude { get; set; }
+    [JsonPropertyName("lng")]
+    public double Longitude { get; set; }
+}
---
+++ WktConverter/WktConverter/WktToGoogleMaps.cs
@@ -0,0 +1,44 @@
+﻿using NetTopologySuite.IO;
+using NetTopologySuite.Geometries;
+
+namespace WktConverter;
+
+public static class WktToGoogleMaps
+{
+    public static List<List<GoogleMapPoint>> ParseWktPolygons(string wkt)
+    {
+        var reader = new WKTReader();
+        var geometry = reader.Read(wkt);
+
+        var result = new List<List<GoogleMapPoint>>();
+
+        if (geometry is Polygon polygon)
+        {
+            result.Add(ExtractCoordinates(polygon.ExteriorRing));
+        }
+        else if (geometry is MultiPolygon multiPolygon)
+        {
+            for (int i = 0; i < multiPolygon.NumGeometries; i++)
+            {
+                var poly = (Polygon)multiPolygon.GetGeometryN(i);
+                result.Add(ExtractCoordinates(poly.ExteriorRing));
+            }
+        }
+        else
+        {
+            throw new NotSupportedException("Solo POLYGON e MULTIPOLYGON sono supportati in questo esempio.");
+        }
+
+        return result;
+    }
+
+    private static List<GoogleMapPoint> ExtractCoordinates(LineString ring)
+    {
+        var coords = new List<GoogleMapPoint>();
+        foreach (var coord in ring.Coordinates)
+        {
+            coords.Add(new GoogleMapPoint { Latitude = coord.Y, Longitude = coord.X });
+        }
+        return coords;
+    }
+}
